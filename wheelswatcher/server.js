1cff693f534b75494e9ef8f18f975a4dabb1324b
“Douglas” <douglas.e.mckinley@gmail.com>
2024-10-12 17:34:00 -0700

    Error querying the database: AxiosError
    code: "ERR_BAD_RESPONSE"
    
    config: {transitional: {silentJSONParsing: true, forcedJSONParsing: true, clarifyTimeoutError: false}, adapter: ["xhr", "http", "fetch"], transformRequest: Array, transformResponse: Array, timeout: 0, …}
    
    message: "Request failed with status code 500"

--- wheelswatcher/server.js
+++ wheelswatcher/server.js
@@ -29,7 +29,7 @@ app.post('/api/query-listings', async (req, res) => {
 
   try {
     const query = `
-      SELECT title, price FROM listings
+      SELECT title,price FROM listings
       WHERE area = $1
       LIMIT 100;
     `;
